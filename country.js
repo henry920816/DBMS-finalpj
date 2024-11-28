e0dffed1ccd71047fb54e8d3f2b5242cc599a97d
henrylin920816 <linqiyao238@gmail.com>
2024-11-28 19:48:43 +0800

    country.js

--- country.js
+++ country.js
@@ -1,54 +1,49 @@
-// search player
 $("#submit").on("click", function(event) {
     event.preventDefault();
-    var option = $("#country-sort-option").val();
-    var order = $("#country-sort-order").val();
-    var str = String.raw`Not Implemented Yet
     
-          ╱|、
-        (˚ˎ 。7  
-         |、˜〵          
-        じしˍ,)ノ`
-    alert(str);
+    var option = $("#country-sort-option").val();  
+    var order = $("#country-sort-order").val();    
+    
+    var url = "country_query.php?m=search&sort=" + option + "&order=" + order;
+    console.log("Request URL: " + url); 
+
+    $("#table-content").load(url);
+
+    $("#country-sort-option").val(option);
+    $("#country-sort-order").val(order);
+    $("#default").remove();  
 });
 
-// load the country selection
 $(window).on("load", function() {
     $("#table-content").load("country_query.php?m=search");
     $("#default").remove();
-})
+});
 
-// display profile
 var players = document.getElementsByClassName("row");
 var profile = document.getElementById("profile");
 var profileContent = document.getElementById("profile-content");
 
-// open profile
-$("#table").on("click", ".row", function(row) {
-    row.currentTarget.classList.add("select");
-    // show profile
+$("#table").on("click", ".row", function(event) {
+    event.currentTarget.classList.add("select");
+    var country = this.dataset.value;  
+
     profile.style.height = "100%";
     profile.style.opacity = "100%";
     profileContent.style.marginTop = "100px";
     profileContent.style.marginBottom = "50px";
     profileContent.style.opacity = "100%";
-    // request query
-    var player = this.cells[0].innerHTML;
-    $("#test").load("country_query.php?m=profile");
-})
 
-// close the profile
+    $("#test").load("country_query.php?m=profile&c=" + country);
+});
+
 window.onclick = function(event) {
     if (event.target == profile) {
-        // unmark as selected
         document.getElementsByClassName("select")[0].classList.remove("select");
-        // close animation
         profile.style.height = "0";
         profile.style.opacity = "0";
         profileContent.style.marginTop = "130px";
         profileContent.style.marginBottom = "20px";
         profileContent.style.opacity = "0";
-        // clear profile
         document.getElementById("test").innerHTML = "";
     }
-}
+};
