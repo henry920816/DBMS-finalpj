76104b9d3fc8d5833fe6d6987b4d7b2f168e5ba1
henrylin920816 <linqiyao238@gmail.com>
2024-12-06 20:10:19 +0800

    year.php

--- year.php
+++ year.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
@@ -13,7 +13,9 @@
 <body>
     <div id="navBarContainer">
         <a class="menuBtn" href="index.php"><i class="material-icons home">home</i></a>
-        <p class="center"><img src="olympics.png" style="width:60px; height:auto; padding-right: 10px"><b>OLYMPICS or smth idk</b></p>
+        <p class="center">
+            <img src="olympics.png" style="width:60px; height:auto; padding-right:10px"><b>OLYMPICS or smth idk</b>
+        </p>
     </div>
 
     <div id="content" style="padding-top: 80px; padding-left: 20px; padding-right: 20px">
@@ -25,19 +27,23 @@
                 <div>
                     <label class="oxygen-bold">Select Type</label>
                     <select id="type-option">
-                        <option value="All">All Olympics</option>
+                        <option value="">All Olympics</option>
                         <option value="Summer">Summer Olympics</option>
                         <option value="Winter">Winter Olympics</option>
                     </select>
                 </div>
-                
                 <div>
                     <label class="oxygen-bold">Select Year</label>
                     <select id="year-option">
                         <option id="year-option-default" value="0">Select Year</option>
                     </select>
                 </div>
-
+                <div>
+                    <label class="oxygen-bold">Select Sport</label>
+                    <select id="sport-option">
+                        <option id="sport-option-default" value="0">Select Sport</option>
+                    </select>
+                </div>
                 <input type="submit" value="Search" id="submit" class="oxygen-bold">
             </form>
         </div>
@@ -46,33 +52,25 @@
             <table>
                 <colgroup>
                     <col style="width: 25%">
-                    <col style="width: 25%">
-                    <col style="width: 25%">
                     <col style="width: 15%">
                     <col style="width: 15%">
                     <col style="width: 15%">
                 </colgroup>
                 <thead>
                     <tr class="attr">
-                        <th>Edition</th>
-                        <th>Sport</th>
                         <th>Event</th>
                         <th style="background-color: #FFE142">Gold</th>
                         <th style="background-color: #C0C0C0">Silver</th>
-                        <th style="background-color: #D18B41">Bronze</th>
+                        <th style="background-color: #D18841">Bronze</th>
                     </tr>
                 </thead>
-                <tbody id="table-content">
-                </tbody>
+                <tbody id="table-content"></tbody>
             </table>
             <div id="default">Search the year of the game to show the list!</div>
         </div>
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
-    </script>
-    <script src="year.js">
-    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="year.js"></script>
 </body>
 </html>
-
