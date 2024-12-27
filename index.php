9d580960ae27eaff9e5244cd68b17b49c9d6bc7b
Mars <ryan0979798230@gmail.com>
2024-12-27 21:05:13 +0800

    Update Title

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olympics</title>
     <link href="style.css" rel="stylesheet" type="text/css">
     <!-- Import Icons -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
